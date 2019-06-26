cd962c74750b0c136ce07048f8b29b38501851f1
xn088430 <duanxinning@xiaoniu66.com>
2019-06-26 23:48:42 +0800

    1

--- app/src/main/java/com/jun/searchedittext/MainActivity.java
+++ app/src/main/java/com/jun/searchedittext/MainActivity.java
@@ -2,11 +2,13 @@ package com.jun.searchedittext;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i("MainActivity", "onCreate");
     }
 }
